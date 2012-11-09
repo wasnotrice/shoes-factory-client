9d09768ba0bee7fdcecec2eeb349267137a2bf7b
Eric Watson <wasnotrice@gmail.com>
2012-11-09 14:43:27 -0600

    Rework specs to use mock API site

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,8 +1,34 @@
-require 'shoes-factory-client'
+require 'shoes-factory/client'
+
+shared_examples "page" do
+  it "is a page" do
+    subject.should be_an_instance_of(Mechanize::Page)
+  end
+end
+
+shared_context "url" do
+  let(:html_path) { File.expand_path "../html", __FILE__ }
+  let(:url) { "file://#{html_path}/index.html" }
+end
 
 describe ShoesFactory::Client do
-  let(:url) { "http://example.com/spec" }
+  include_context "url"
   subject { ShoesFactory::Client.new(url) }
 
   its(:root_url) { should eq(url) }
+end
+
+describe "API pages" do
+  include_context "url"
+  let(:client) { ShoesFactory::Client.new(url) }
+
+  describe "#root" do
+    subject { client.root }
+    it_behaves_like 'page'
+  end
+
+  describe "#downloads" do
+    subject { client.root }
+    it_behaves_like 'page'
+  end
 end

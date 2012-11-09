e29cfd25f8056376a61e0acf1f2adc5382b0e283
Eric Watson <wasnotrice@gmail.com>
2012-11-09 14:42:03 -0600

    Remove unused file

--- spec/client.rb
+++
@@ -1,8 +0,0 @@
-require 'shoes-factory/client'
-
-describe ShoesFactory::Client do
-  let(:url) { "localhost:54035" }
-  subject { ShoesFactory::Client.new(url) }
-
-  its(:root_url) { should eq(url) }
-end
